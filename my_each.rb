3acbe2524605ec7711ac67311a647b6bdae023c8
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 04:28:05 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1,2,3,4]
+
+def my_each(array)
+ if block_given? then
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
+array
+end
+
